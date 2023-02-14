87a8150bff72e5490fcaf8c3fda89f1e27bd13a8
Chenna Sreenu <Chenna.Sreenu@ril.com>
2023-02-14 14:11:13 +0530

    aa

--- app.js
+++ app.js
@@ -3,22 +3,23 @@ if ('OTPCredential' in window) {
     const input = document.querySelector('input[autocomplete="one-time-code"]');
     if (!input) return;
     const ac = new AbortController();
-
+    const form = input.closest('form');
+    if (form) {
+      form.addEventListener('submit', (e) => {
+        ac.abort();
+      });
+    }
     navigator.credentials
       .get({
         otp: { transport: ['sms'] },
         signal: ac.signal,
       })
       .then((otp) => {
-        alert(otp.code);
         input.value = otp.code;
+        if (form) form.submit();
       })
       .catch((err) => {
         alert(err.toString());
-      })
-      .finally(() => {
-        ac.abort();
-        alert('Error');
-      });
+      }).finally;
   });
 }
