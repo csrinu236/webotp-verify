49605ef9f6e20e306d7748b8d28e3fe135e1b122
Chenna Sreenu <Chenna.Sreenu@ril.com>
2023-02-14 14:16:52 +0530

    helloes

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 if ('OTPCredential' in window) {
   window.addEventListener('DOMContentLoaded', (e) => {
-    console.log('hello');
+    console.log('hello1');
     const input = document.querySelector('input[autocomplete="one-time-code"]');
     if (!input) return;
+    console.log('hello2');
+
     const ac = new AbortController();
     const form = input.closest('form');
     if (form) {
       form.addEventListener('submit', (e) => {
+        console.log('hello3');
+
         ac.abort();
       });
     }
@@ -16,14 +20,20 @@ if ('OTPCredential' in window) {
         signal: ac.signal,
       })
       .then((otp) => {
+        console.log('hello4');
+
         input.value = otp.code;
         if (form) form.submit();
       })
       .catch((err) => {
+        console.log('hello5');
+
         alert(err.toString());
       })
       .finally((err) => {
+        console.log('hello6');
         alert(err.toString());
       });
+    console.log('hello7');
   });
 }
